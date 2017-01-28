210244ab66c0de5c45661e077153830a4b8f7ef8
Tarek Morshed <tarekmorshed@Tarek-2.local>
2017-01-28 17:25:41 +0200

    add photo completed

--- photoGallery/Addphoto.swift
+++ photoGallery/Addphoto.swift
@@ -2,28 +2,51 @@
 
 import UIKit
 
-class Addphoto: UIViewController {
+class Addphoto: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
     
     @IBOutlet weak var titleTextFiled: UITextField!
     @IBOutlet weak var photo: UIImageView!
     
+    var imagePicker = UIImagePickerController()
  
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        imagePicker.delegate = self
     }
     
     @IBAction func gallerySelect(_ sender: Any) {
-        print("gallery select")
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        photo.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func cameraButton(_ sender: Any) {
         print("Camera select")
     }
     @IBAction func addButton(_ sender: Any) {
-        print("Add a new photo")
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let imageData = ImageData(context: context)
+        
+        imageData.title = titleTextFiled?.text
+        
+        imageData.image = UIImagePNGRepresentation(photo.image!) as NSData?
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+   
     }
 }
